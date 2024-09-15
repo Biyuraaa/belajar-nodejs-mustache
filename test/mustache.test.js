d623d90debe3fdf0e3f0d475b1239f27df573ef0
M Bimo Bayu Bagaskara <m.bimo.bayu.bagaskara-2022@fst.unair.ac.id>
2024-09-15 11:22:41 +0700

    Mustache Cache

--- test/mustache.test.js
+++ test/mustache.test.js
@@ -6,3 +6,12 @@ test("Test Mustache", () => {
   });
   expect(response).toBe("Hello, M Bimo Bayu Bagaskara");
 });
+
+test("Test Mustache Cache", () => {
+  Mustache.parse("Hello, {{name}}");
+  const data = Mustache.render("Hello, {{name}}", {
+    name: "M Bimo Bayu Bagaskara",
+  });
+
+  expect(data).toBe("Hello, M Bimo Bayu Bagaskara");
+});
